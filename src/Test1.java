a57ba7b0aa63a9c3198d5451c9cd90da3debc851
LookRain <brantscube@hotmail.com>
2015-09-19 23:47:33 +0800

    added more Junit test cases for sort()

--- src/Test1.java
+++ src/Test1.java
@@ -9,14 +9,28 @@ public class Test1 {
 
 	@Test
 	public void test() throws IOException {
-		testOneCommand("add cmd", "added to : \"task c\"", "add task c");
+		testOneCommand("add cmd1", "added to : \"task c\"", "add task c");
 		testOneCommand("add invalid cmd", "Invalid command, please type again", "aaadd rampage");
-		testOneCommand("add cmd", "added to : \"task b\"", "add task b");
-		testOneCommand("add cmd", "added to : \"task a\"", "add task a");
+		testOneCommand("add cmd2", "added to : \"task b\"", "add task b");
+		testOneCommand("add cmd3", "added to : \"task a\"", "add task a");
 		assertEquals("test display", "1. task c" + System.lineSeparator() + "2. task b" + System.lineSeparator() + "3. task a", TextBuddy.getList());
 		testOneCommand("sort cmd", "All tasks have been sorted in alphabetical order", "sort");
-		assertEquals("test result after sort", "1. task a" + System.lineSeparator() + "2. task b" + System.lineSeparator() + "3. task c", TextBuddy.getList());
-		
+		assertEquals("test result after sort", "1. task a" + 
+				System.lineSeparator() + "2. task b" + 
+				System.lineSeparator() + "3. task c", 
+				TextBuddy.getList());
+		testOneCommand("add cmd4", "added to : \"happy tree friends\"", "add happy tree friends");
+		testOneCommand("add cmd5", "added to : \"we bare bears\"", "add we bare bears");
+		testOneCommand("add cmd6", "added to : \"happieness\"", "add happieness");
+		testOneCommand("sort cmd", "All tasks have been sorted in alphabetical order", "sort");
+		assertEquals("test result after sort", "1. happieness" + 
+				System.lineSeparator() + "2. happy tree friends"+
+				System.lineSeparator() + "3. task a" +
+				System.lineSeparator() + "4. task b" +
+				System.lineSeparator() + "5. task c" +
+				System.lineSeparator() + "6. we bare bears",
+				TextBuddy.getList());
+
 	}
 	private void testOneCommand(String description, String expected, String command) throws IOException {
 		assertEquals(description, expected, TextBuddy.executeCmd(command)); 
